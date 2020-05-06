f9eb82efe851c48e71a5ae9da4e9fdd5e2967977
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-05-06 22:06:33 +0200

    Implementation of Dockerfile and provisional deletion of camera Class

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:latest
+
+LABEL maintainer="ericghoubiguian@live.fr"
+
+COPY . /opencv_webserver
+
+WORKDIR /opencv_webserver
+
+RUN apt upgrade -y
+
+RUN apt update -y
+
+RUN apt install python3-pip -y
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+ENTRYPOINT ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 from flask import Flask, render_template, Response
-from camera import Camera
 
 app = Flask(__name__)
-camera = Camera()
-
-def display_camera():
-
-	while True:
-
-		frame = camera.get_frame()
-
-		yield (b'--frame\r\n'
-		       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,11 +10,6 @@ def home():
 	phrase_comme_tableau = ["Hello", "World", "My", "master", "will", "rule", "you", "for", "ever", "Hahahaha"]
 
 	return render_template('main.html', titre="Bienvenue !", phrase_comme_tableau=phrase_comme_tableau, phrase_comme_string=phrase_comme_string)
-
-@app.route('/video_displaying', methods=['GET'])
-def video_display():
-
-	return Response(display_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 
