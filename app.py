328474b457d0687639a88ec62588a1ceef5805bb
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-05-10 15:06:00 +0200

    Defining the handlers for 404 and 500 errors

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found_function(e):
+
+	return "<h1 style='color: red;'>Error 404: Page not found, sorry...</h1>", 404
+
+@app.errorhandler(500)
+def internal_problem_function(e):
+
+	return "<h1 style='color: red;'>Error 500: Internal problem, sorry...</h1>", 500
+
 @app.route('/', methods=['GET'])
 def home():
 
@@ -9,7 +19,7 @@ def home():
 
 	phrase_comme_tableau = ["Hello", "World", "My", "master", "will", "rule", "you", "for", "ever", "Hahahaha"]
 
-	return render_template('main.html', titre="Bienvenue !", phrase_comme_tableau=phrase_comme_tableau, phrase_comme_string=phrase_comme_string)
+	return render_template('main.html', titre="Bienvenue !", phrase_comme_tableau=phrase_comme_tableau, phrase_comme_string=phrase_comme_string), 200
 
 if __name__ == '__main__':
 
